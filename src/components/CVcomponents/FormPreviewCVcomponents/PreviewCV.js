86a8bbced73db1c22fb1a77dedbccabd232a94e5
mrjackmaster <jacektrg+github@gmail.com>
2021-08-20 21:52:00 +0200

    Layout of preview CV sheet.

---
+++ src/components/CVcomponents/FormPreviewCVcomponents/PreviewCV.js
@@ -0,0 +1,56 @@
+function PreviewCV() {
+	return (
+		<>
+			<div className='headerCV'>
+				<div className='personalInformationCV'>
+					<h1>Name</h1>
+					<h2>Title</h2>
+				</div>
+			</div>
+			<div className='mainBodyCV'>
+				<div className='leftPanelCV'>
+					<div>
+						<h3>Photo</h3>
+						<p>000</p>
+					</div>
+					<div>
+						<h3>Date of Birth</h3>
+						<p>111</p>
+					</div>
+					<div>
+						<h3>Address</h3>
+						<p>222</p>
+					</div>
+					<div>
+						<h3>Phone Number</h3>
+						<p>333</p>
+					</div>
+					<div>
+						<h3>Email</h3>
+						<p>444</p>
+					</div>
+				</div>
+				<div className='rightPanelCV'>
+					<div className='descriptionCV'>
+						<h3>Description</h3>
+						<p>Description</p>
+					</div>
+					<div className='experienceCV'>
+						<h3>Experience</h3>
+						<p>Experience</p>
+						<p>Experience</p>
+						<p>Experience</p>
+					</div>
+					<div className='educationCV'>
+						<h3>Education</h3>
+						<p>Education</p>
+						<p>Education</p>
+						<p>Education</p>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default PreviewCV;
