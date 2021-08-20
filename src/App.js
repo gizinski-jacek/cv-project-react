708e7e6b0b7a686a8468b2a843fde4664b26d802
mrjackmaster <jacektrg+github@gmail.com>
2021-08-20 21:44:36 +0200

    App state for controlling which input mode to display. For now only FormPreview is working.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './App.css';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
-	return <div></div>;
+	const [inputMode, setInputMode] = useState('direct');
+
+	function changeInputMode(e) {
+		setInputMode((prevState) => e.target.id);
+	}
+
+	return (
+		<>
+			<Header inputMode={inputMode} changeInputMode={changeInputMode} />
+			<Main inputMode={inputMode} />
+			<Footer />
+		</>
+	);
 }
 
 export default App;
