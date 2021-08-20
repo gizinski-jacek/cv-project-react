9a7e94556246ea77e6ee85762c8723f3e7b93eeb
mrjackmaster <jacektrg+github@gmail.com>
2021-08-20 21:46:54 +0200

    Header with controls to switch between data input mode, saving data to local storage* and clearing form*. * - not yet functional.

---
+++ src/components/Header.js
@@ -0,0 +1,45 @@
+function Header(props) {
+	return (
+		<div className='header'>
+			<h1>CV Creator</h1>
+			<div className='inputMode'>
+				{props.inputMode === 'direct' ? (
+					<div>
+						<button
+							id='direct'
+							className='activeInputMode'
+							onClick={props.changeInputMode}
+						>
+							Direct Edit
+						</button>
+						<button
+							id='formPreview'
+							onClick={props.changeInputMode}
+						>
+							{'Form & Preview'}
+						</button>
+					</div>
+				) : (
+					<div>
+						<button id='direct' onClick={props.changeInputMode}>
+							Direct Edit
+						</button>
+						<button
+							id='formPreview'
+							className='activeInputMode'
+							onClick={props.changeInputMode}
+						>
+							{'Form & Preview'}
+						</button>
+					</div>
+				)}
+			</div>
+			<div className='CVcontrols'>
+				<button id='save'>Save</button>
+				<button id='clear'>Clear</button>
+			</div>
+		</div>
+	);
+}
+
+export default Header;
