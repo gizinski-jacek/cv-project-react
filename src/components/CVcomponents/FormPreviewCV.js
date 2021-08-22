3f5cd9e7e3a5c1ebbf729641e1dd47f4d3420d07
mrjackmaster <jacektrg+github@gmail.com>
2021-08-22 14:23:11 +0200

    Functions for handling onChange in child comps and state for storing it. Will probably refactor this soon.

--- src/components/CVcomponents/FormPreviewCV.js
+++ src/components/CVcomponents/FormPreviewCV.js
@@ -1,18 +1,96 @@
+import { useState } from 'react';
 import PreviewCV from './FormPreviewCVcomponents/PreviewCV';
 import PersonalInformation from './FormPreviewCVcomponents/PersonalInformation';
 import Experience from './FormPreviewCVcomponents/Experience';
 import Education from './FormPreviewCVcomponents/Education';
 
 function FormPreviewCV() {
+	const [allCVData, setAllCVData] = useState({
+		personal: {
+			firstName: '',
+			lastName: '',
+			title: '',
+			photo: '',
+			birth: '2020-01-01',
+			address: '',
+			phone: '',
+			email: '',
+			description: '',
+		},
+		experience: {
+			company: '',
+			position: '',
+			city: '',
+			workStarted: '2000-01-01',
+			workEnded: '2020-01-01',
+		},
+		education: {
+			university: '',
+			universityCity: '',
+			degree: '',
+			subject: '',
+			universityStarted: '2000-01-01',
+			universityEnded: '2020-01-01',
+		},
+	});
+
+	function handlePersonal(e) {
+		const { name, value } = e.target;
+		setAllCVData((prevState) => ({
+			...prevState,
+			personal: {
+				...prevState.personal,
+				[name]: value,
+			},
+		}));
+	}
+
+	function handleExperience(e) {
+		const { name, value } = e.target;
+		setAllCVData((prevState) => ({
+			...prevState,
+			experience: {
+				...prevState.experience,
+				[name]: value,
+			},
+		}));
+	}
+
+	function handleEducation(e) {
+		const { name, value } = e.target;
+		setAllCVData((prevState) => ({
+			...prevState,
+			education: {
+				...prevState.education,
+				[name]: value,
+			},
+		}));
+	}
+
+	// function handleNewSection(e) {
+	// 	e.preventDefault();
+	// 	const { name, value } = e.target;
+	// 	console.log(e.target);
+	// }
+
 	return (
 		<div className='formPreviewCV'>
 			<div className='previewCV'>
-				<PreviewCV />
+				<PreviewCV data={allCVData} />
 			</div>
 			<div className='formCV'>
-				<PersonalInformation />
-				<Experience />
-				<Education />
+				<PersonalInformation
+					data={allCVData.personal}
+					handleInput={handlePersonal}
+				/>
+				<Experience
+					data={allCVData.experience}
+					handleInput={handleExperience}
+				/>
+				<Education
+					data={allCVData.education}
+					handleInput={handleEducation}
+				/>
 			</div>
 		</div>
 	);
