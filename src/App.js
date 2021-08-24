b8a7d1710f34ee05c7e20afbb775c5679cf63a27
mrjackmaster <jacektrg+github@gmail.com>
2021-08-24 18:18:13 +0200

    Loading empty CV data. Replacing it with data from localStorage if it exists. Function for clearing local storage. Still need function to get data allCVData from children and save it to LS.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import emptyCV from './components/CVcomponents/emptyCV';
 
 function App() {
 	const [inputMode, setInputMode] = useState('direct');
+	const [savedData, setSavedData] = useState(emptyCV);
 
 	function changeInputMode(e) {
 		setInputMode((prevState) => e.target.id);
 	}
 
+	function saveDataToLS() {
+		console.log(1);
+	}
+
+	function clearAllData() {
+		localStorage.clear();
+	}
+
+	useEffect(() => {
+		let localCVData = localStorage.getItem('savedData');
+		if (localCVData) {
+			setSavedData((prevState) => localCVData);
+		}
+	}, []);
+
 	return (
 		<>
-			<Header inputMode={inputMode} changeInputMode={changeInputMode} />
-			<Main inputMode={inputMode} />
+			<Header
+				inputMode={inputMode}
+				changeInputMode={changeInputMode}
+				saveData={saveDataToLS}
+				clearData={clearAllData}
+			/>
+			<Main inputMode={inputMode} savedData={savedData} />
 			<Footer />
 		</>
 	);
