ff687f8768ce27b231109bfe66903d934977165d
mrjackmaster <jacektrg+github@gmail.com>
2021-08-24 18:15:42 +0200

    Passing down CV data from parent.

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,11 @@ import FormPreviewCV from './CVcomponents/FormPreviewCV';
 function Main(props) {
 	return (
 		<div className='main'>
-			{props.inputMode === 'direct' ? <DirectCV /> : <FormPreviewCV />}
+			{props.inputMode === 'direct' ? (
+				<DirectCV data={props.data} />
+			) : (
+				<FormPreviewCV savedData={props.savedData} />
+			)}
 		</div>
 	);
 }
