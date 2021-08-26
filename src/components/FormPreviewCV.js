fb1bd343e444dbc8ac3eae914984774ab0536cb7
mrjackmaster <jacektrg+github@gmail.com>
2021-08-26 17:02:00 +0200

    Lifting up state and handlers to App. Destructuring props and updating passed down props.

--- src/components/FormPreviewCV.js
+++ src/components/FormPreviewCV.js
@@ -1,148 +1,43 @@
-import { useEffect, useState } from 'react';
 import PreviewCV from './FormPreviewCVcomponents/PreviewCV';
 import PersonalInformation from './FormPreviewCVcomponents/PersonalInformation';
 import Experience from './FormPreviewCVcomponents/Experience';
 import Education from './FormPreviewCVcomponents/Education';
-import { nanoid } from 'nanoid';
 
 function FormPreviewCV(props) {
-	const { savedCVData, requestSave, saveDataToLS } = props;
-
-	useEffect(() => saveDataToLS(allCVData), [requestSave]);
-
-	const [allCVData, setAllCVData] = useState(savedCVData);
-
-	function handlePersonalChange(e) {
-		const { name, value } = e.target;
-		setAllCVData((prevState) => ({
-			...prevState,
-			personal: {
-				...prevState.personal,
-				[name]: value,
-			},
-		}));
-	}
-
-	function handleFileChange(e) {
-		const { name } = e.target;
-		const file = e.target.files[0];
-		if (!file) {
-			return;
-		}
-		const reader = new FileReader();
-		reader.onload = () => {
-			setAllCVData((prevState) => ({
-				...prevState,
-				personal: {
-					...prevState.personal,
-					[name]: reader.result,
-				},
-			}));
-		};
-		reader.readAsDataURL(file);
-	}
-
-	function handleExperienceChange(e, id) {
-		const { name, value } = e.target;
-		setAllCVData((prevState) => {
-			const newState = prevState.experience.map((item) => {
-				if (item.id === id) {
-					return { ...item, [name]: value };
-				}
-				return item;
-			});
-			return { ...prevState, experience: [...newState] };
-		});
-	}
-
-	function handleAddExperience(e) {
-		e.preventDefault();
-		setAllCVData((prevState) => ({
-			...prevState,
-			experience: [
-				...prevState.experience,
-				{
-					id: nanoid(),
-					company: '',
-					position: '',
-					city: '',
-					workStart: '',
-					workEnd: '',
-				},
-			],
-		}));
-	}
-
-	function handleRemoveExperience(e, id) {
-		e.preventDefault();
-		setAllCVData((prevState) => ({
-			...prevState,
-			experience: prevState.experience.filter((item) => item.id !== id),
-		}));
-	}
-
-	function handleEducationChange(e, id) {
-		const { name, value } = e.target;
-		setAllCVData((prevState) => {
-			const newState = prevState.education.map((item) => {
-				if (item.id === id) {
-					return { ...item, [name]: value };
-				}
-				return item;
-			});
-			return { ...prevState, education: [...newState] };
-		});
-	}
-
-	function handleAddEducation(e) {
-		e.preventDefault();
-		setAllCVData((prevState) => ({
-			...prevState,
-			education: [
-				...prevState.education,
-				{
-					id: nanoid(),
-					university: '',
-					uniCity: '',
-					degree: '',
-					subject: '',
-					uniStart: '',
-					uniEnd: '',
-				},
-			],
-		}));
-	}
-
-	function handleRemoveEducation(e, id) {
-		e.preventDefault();
-		setAllCVData((prevState) => ({
-			...prevState,
-			education: prevState.education.filter((item) => item.id !== id),
-		}));
-	}
+	const {
+		data,
+		changePersonal,
+		changeFile,
+		changeExperience,
+		addExperience,
+		removeExperience,
+		changeEducation,
+		addEducation,
+		removeEducation,
+	} = props;
 
 	return (
 		<div className='formPreviewCV'>
 			<div className='previewCV'>
-				<PreviewCV data={allCVData} />
+				<PreviewCV data={data} />
 			</div>
 			<div className='formCV'>
 				<PersonalInformation
-					data={allCVData.personal}
-					handleChange={handlePersonalChange}
-					handleFile={handleFileChange}
+					data={data.personal}
+					handleChange={changePersonal}
+					handleFile={changeFile}
 				/>
 				<Experience
-					data={allCVData.experience}
-					handleChange={handleExperienceChange}
-					handleAdd={handleAddExperience}
-					handleRemove={handleRemoveExperience}
+					data={data.experience}
+					handleChange={changeExperience}
+					handleAdd={addExperience}
+					handleRemove={removeExperience}
 				/>
 				<Education
-					data={allCVData.education}
-					handleChange={handleEducationChange}
-					handleAdd={handleAddEducation}
-					handleRemove={handleRemoveEducation}
+					data={data.education}
+					handleChange={changeEducation}
+					handleAdd={addEducation}
+					handleRemove={removeEducation}
 				/>
 			</div>
 		</div>
