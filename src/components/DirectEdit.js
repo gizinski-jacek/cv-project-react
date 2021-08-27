6fe62dfc36b7cadaec48edde83a6327cad48026f
mrjackmaster <jacektrg+github@gmail.com>
2021-08-27 23:12:13 +0200

    Quite a messy way to handle the toggling of editing personal data fields. Will try to find better way of doing this.

--- src/components/DirectEdit.js
+++ src/components/DirectEdit.js
@@ -1,17 +1,61 @@
-function DirectEdit(props) {
-	const {
-		data,
-		changePersonal,
-		changeFile,
-		changeExperience,
-		addExperience,
-		removeExperience,
-		changeEducation,
-		addEducation,
-		removeEducation,
-	} = props;
+import { useState } from 'react';
+import OverviewEdit from './DirectEditComponents/OverviewEdit';
 
-	return <div className='directEdit'></div>;
+function DirectEdit(props) {
+	const [editing, setEditing] = useState({
+		personal: false,
+		experience: false,
+		education: false,
+	});
+
+	function toggleEditsOff(e) {
+		e.stopPropagation();
+		setEditing({
+			personal: false,
+			experience: false,
+			education: false,
+		});
+	}
+
+	function toggleEdit(e, id) {
+		e.stopPropagation();
+		console.log(e.target.closest('div'));
+
+		setEditing({
+			personal: false,
+			experience: false,
+			education: false,
+		});
+		if (id) {
+			const section = e.currentTarget.parentElement.className;
+			setEditing((prevState) => ({
+				...prevState,
+				[section]: true,
+			}));
+		} else {
+			if (
+				e.currentTarget.className !== 'rightPanelCV' ||
+				e.target.closest('div').className === 'description'
+			)
+				setEditing((prevState) => ({
+					...prevState,
+					personal: true,
+				}));
+		}
+	}
+
+	return (
+		<div className='directEditMode' onClick={toggleEditsOff}>
+			<div className='overview'>
+				<OverviewEdit
+					{...props.data}
+					{...props.dataHandlers}
+					editing={editing}
+					toggle={toggleEdit}
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default DirectEdit;
