7671d6e7b29db9665a4fdd6e3e5612fc53c354cc
mrjackmaster <jacektrg+github@gmail.com>
2021-08-29 18:53:06 +0200

    Adding placeholders to empty CV fields.

--- src/components/FormOverviewComponents/Overview.js
+++ src/components/FormOverviewComponents/Overview.js
@@ -2,42 +2,63 @@ function Overview(props) {
 	const { personal, experience, education } = props.data;
 
 	const expDisplay = experience.map((item) => {
+		let expStartEndDate;
+		if (experience.startDate || education.endDate) {
+			expStartEndDate = experience.startDate.startDate(
+				' - ',
+				education.endDate
+			);
+		}
 		return (
 			<div key={item.id} className='item'>
-				<span className='date'>
-					{item.workStart} - {item.workEnd}
+				<span className='date' placeholder_text='Start Date - End Date'>
+					{expStartEndDate}
 				</span>
-				<p>{item.company}</p>
-				<p>{item.city}</p>
-				<p>{item.position}</p>
+				<p placeholder_text='Company'>{item.company}</p>
+				<p placeholder_text='City'>{item.city}</p>
+				<p placeholder_text='Position'>{item.position}</p>
 				<hr className='grayHR' />
 			</div>
 		);
 	});
 
 	const eduDisplay = education.map((item) => {
+		let eduStartEndDate;
+		if (education.startDate || education.endDate) {
+			eduStartEndDate = education.startDate.concat(
+				' - ',
+				education.endDate
+			);
+		}
 		return (
 			<div key={item.id} className='item'>
-				<span className='date'>
-					{item.uniStart} - {item.uniEnd}
+				<span className='date' placeholder_text='Start Date - End Date'>
+					{eduStartEndDate}
 				</span>
-				<p>{item.university}</p>
-				<p>{item.uniCity}</p>
-				<p>Degree: {item.degree}</p>
-				<p>Subject: {item.subject}</p>
+				<p placeholder_text='University'>{item.university}</p>
+				<p placeholder_text='City'>{item.uniCity}</p>
+				<p placeholder_text='Degree:'>
+					{item.degree ? 'Degree: ' + item.degree : null}
+				</p>
+				<p placeholder_text='Subject:'>
+					{item.subject ? 'Subject: ' + item.subject : null}
+				</p>
 				<hr className='grayHR' />
 			</div>
 		);
 	});
 
+	let fullName;
+	if (personal.firstName || personal.lastName) {
+		fullName = personal.firstName.concat(' ', personal.lastName);
+	}
+
 	return (
 		<>
 			<div className='headerCV'>
-				<h1>
-					{personal.firstName} {personal.lastName}
-				</h1>
+				<h1 placeholder_text='Full Name'>{fullName}</h1>
 				<hr className='blackHR' />
-				<h2>{personal.title}</h2>
+				<h2 placeholder_text='Title'>{personal.title}</h2>
 			</div>
 			<div className='mainCVBody'>
 				<div className='leftPanelCV'>
@@ -47,29 +68,31 @@ function Overview(props) {
 					<div>
 						<h3>Date of Birth</h3>
 						<hr className='blackHR' />
-						<p>{personal.birth}</p>
+						<p placeholder_text='YYYY-MM-DD'>{personal.birth}</p>
 					</div>
 					<div>
 						<h3>Address</h3>
 						<hr className='blackHR' />
-						<p>{personal.address}</p>
+						<p placeholder_text='Address'>{personal.address}</p>
 					</div>
 					<div>
 						<h3>Phone Number</h3>
 						<hr className='blackHR' />
-						<p>{personal.phone}</p>
+						<p placeholder_text='Phone Number'>{personal.phone}</p>
 					</div>
 					<div>
 						<h3>Email</h3>
 						<hr className='blackHR' />
-						<p>{personal.email}</p>
+						<p placeholder_text='Email'>{personal.email}</p>
 					</div>
 				</div>
 				<div className='rightPanelCV'>
 					<div className='description'>
 						<h3>Description</h3>
 						<hr className='blackHR' />
-						<p>{personal.description}</p>
+						<p placeholder_text='Your description'>
+							{personal.description}
+						</p>
 					</div>
 					<div className='experience'>
 						<h3>Experience</h3>
