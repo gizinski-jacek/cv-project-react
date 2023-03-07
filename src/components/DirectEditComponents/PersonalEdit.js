38aae7f7b7bb9b018f30bcf81803e03c7702703e
Jacek Gizinski <jacektrg+github@gmail.com>
2023-03-07 22:13:23 +0100

    Destructuring new functions from props.
    Conditionally rendering removable sections.
    Conditionally rendering buttons for removing/adding sections.
    Adding mouse enter/leave listeners to trigger functions.
    Updating/adding classes.
    Updating min/max attributes in date inputs.
    Updating textarea name property.

--- src/components/DirectEditComponents/PersonalEdit.js
+++ src/components/DirectEditComponents/PersonalEdit.js
@@ -1,14 +1,22 @@
-function ExperienceEdit(props) {
-	const {
-		personal,
-		handleChange,
-		handleFile,
-		experienceItems,
-		educationItems,
-	} = props;
-
+function PersonalEdit({
+	personal,
+	handleChange,
+	handleFile,
+	experienceItems,
+	educationItems,
+	removePersonalSection,
+	addPersonalSection,
+	addExperience,
+	addEducation,
+	addSection,
+	removeSection,
+	showSectionBtns,
+	hideSectionBtns,
+	showButtons,
+	hideButtons,
+}) {
 	return (
-		<>
+		<div className='mainCV'>
 			<div className='headerCV editing'>
 				<h1>
 					<input
@@ -27,50 +35,106 @@ function ExperienceEdit(props) {
 					/>
 				</h1>
 				<hr className='blackHR' />
-				<input
-					type='text'
-					name='title'
-					placeholder='Title'
-					value={personal.title}
-					onChange={handleChange}
-				/>
-			</div>
-			<div className='mainCVBody'>
-				<div className='leftPanelCV editing'>
-					<div>
+				<h1>
+					{personal.title !== undefined ? (
+						<>
+							<input
+								type='text'
+								name='title'
+								placeholder='Title'
+								value={personal.title}
+								onChange={handleChange}
+							/>
+							<button
+								type='button'
+								className='removePersonalSectionBtnInCV'
+								onClick={() => removePersonalSection('title')}
+							>
+								Remove title
+							</button>
+						</>
+					) : (
 						<button
-							onClick={() => {
-								document.getElementById('photoFile').click();
-							}}
+							type='button'
+							className='addPersonalSectionBtnInCV'
+							onClick={() => addPersonalSection('title')}
 						>
-							<img
-								src={personal.photo}
-								alt=''
+							Add title
+						</button>
+					)}
+				</h1>
+			</div>
+			<div className='cvBody'>
+				<div className='leftPanelCV editing'>
+					{personal.photo !== undefined ? (
+						<div>
+							<button
+								className='photoBtn'
+								onClick={() => {
+									document.getElementById('photoFile')?.click();
+								}}
+							>
+								<img
+									src={personal.photo}
+									alt=''
+									name='photo'
+									onChange={handleFile}
+								/>
+							</button>
+							<input
+								type='file'
+								id='photoFile'
 								name='photo'
+								placeholder='Photo'
+								accept='.png,.jpeg,.jpg'
 								onChange={handleFile}
 							/>
+							<button
+								type='button'
+								className='removePersonalSectionBtnInCV'
+								onClick={() => removePersonalSection('photo')}
+							>
+								Remove photo
+							</button>
+						</div>
+					) : (
+						<button
+							type='button'
+							className='addPersonalSectionBtnInCV'
+							onClick={() => addPersonalSection('photo')}
+						>
+							Add photo
 						</button>
-						<input
-							type='file'
-							id='photoFile'
-							name='photo'
-							placeholder='Photo'
-							accept='.png,.jpeg,.jpg'
-							onChange={handleFile}
-						/>
-					</div>
-					<div>
-						<h3>Date of Birth</h3>
-						<hr className='blackHR' />
-						<input
-							type='date'
-							name='birth'
-							min='1950-01-01'
-							max='2050-12-31'
-							value={personal.birth}
-							onChange={handleChange}
-						/>
-					</div>
+					)}
+					{personal.birth !== undefined ? (
+						<div>
+							<h3>Date of Birth</h3>
+							<hr className='blackHR' />
+							<input
+								type='date'
+								name='birth'
+								min={`${new Date().getFullYear() - 100}-01-01`}
+								max={`${new Date().getFullYear() + 100}-12-31`}
+								value={personal.birth}
+								onChange={handleChange}
+							/>
+							<button
+								type='button'
+								className='removePersonalSectionBtnInCV'
+								onClick={() => removePersonalSection('birth')}
+							>
+								Remove birth date
+							</button>
+						</div>
+					) : (
+						<button
+							type='button'
+							className='addPersonalSectionBtnInCV'
+							onClick={() => addPersonalSection('birth')}
+						>
+							Add birth date
+						</button>
+					)}
 					<div>
 						<h3>Address</h3>
 						<hr className='blackHR' />
@@ -105,33 +169,118 @@ function ExperienceEdit(props) {
 						/>
 					</div>
 				</div>
-				<div className='rightPanelCV'>
-					<div className='description editing'>
-						<h3>Description</h3>
-						<hr className='blackHR' />
-						<textarea
-							type='text'
-							name='description'
-							rows='3'
-							placeholder='Description'
-							value={personal.description}
-							onChange={handleChange}
-						/>
-					</div>
-					<div className='experience'>
-						<h3>Experience</h3>
-						<hr className='blackHR' />
-						{experienceItems}
-					</div>
-					<div className='education'>
-						<h3>Education</h3>
-						<hr className='blackHR' />
-						{educationItems}
-					</div>
+				<div
+					className='rightPanelCV'
+					onMouseEnter={showButtons}
+					onMouseLeave={hideButtons}
+				>
+					{personal.profile !== undefined ? (
+						<div className='profile editing'>
+							<h3>Profile</h3>
+							<hr className='blackHR' />
+							<textarea
+								type='text'
+								name='profile'
+								rows='3'
+								placeholder='Profile'
+								value={personal.profile}
+								onChange={handleChange}
+							/>
+							<button
+								type='button'
+								className='removePersonalSectionBtnInCV'
+								onClick={() => removePersonalSection('profile')}
+							>
+								Remove profile field
+							</button>
+						</div>
+					) : (
+						<button
+							type='button'
+							className='addPersonalSectionBtnInCV'
+							onClick={() => addPersonalSection('profile')}
+						>
+							Add profile field
+						</button>
+					)}
+					{experienceItems ? (
+						<div
+							className='experience'
+							onMouseEnter={showSectionBtns}
+							onMouseLeave={hideSectionBtns}
+						>
+							<h3>Experience</h3>
+							<div id='expSectionBtns'>
+								<button
+									className='addEntryBtnInCV'
+									type='button'
+									onClick={addExperience}
+								>
+									Add entry
+								</button>
+								<button
+									className='removeSectionBtnInCV'
+									type='button'
+									onClick={() => removeSection('experience')}
+								>
+									Remove section
+								</button>
+							</div>
+							<hr className='blackHR' />
+							{experienceItems}
+						</div>
+					) : (
+						<button
+							id='addExpSectionBtnInCV'
+							type='button'
+							onClick={() => addSection('experience')}
+						>
+							Add experience section
+						</button>
+					)}
+					{educationItems ? (
+						<div
+							className='education'
+							onMouseEnter={showSectionBtns}
+							onMouseLeave={hideSectionBtns}
+						>
+							<h3>Education</h3>
+							<div
+								id='eduSectionBtns'
+								onMouseEnter={showSectionBtns}
+								onMouseLeave={hideSectionBtns}
+							>
+								<button
+									className='addEntryBtnInCV'
+									type='button'
+									onClick={addEducation}
+								>
+									Add entry
+								</button>
+								<button
+									className='removeSectionBtnInCV'
+									type='button'
+									onClick={() => removeSection('education')}
+								>
+									Remove section
+								</button>
+							</div>
+							<hr className='blackHR' />
+							{educationItems}
+						</div>
+					) : (
+						<button
+							id='addEduSectionBtnInCV'
+							type='button'
+							onClick={() => addSection('education')}
+						>
+							Add education section
+						</button>
+					)}
 				</div>
 			</div>
-		</>
+		</div>
 	);
 }
 
-export default ExperienceEdit;
+export default PersonalEdit;
